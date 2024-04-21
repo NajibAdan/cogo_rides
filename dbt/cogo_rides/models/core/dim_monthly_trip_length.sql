b17b787883fb0afd27a07d46c6653e5faf346fa2
Najib Adan <najibadan@outlook.jp>
2024-04-21 10:02:10 +0000

    group by rides & member type

--- dbt/cogo_rides/models/core/dim_monthly_trip_length.sql
+++ dbt/cogo_rides/models/core/dim_monthly_trip_length.sql
@@ -11,12 +11,15 @@ select
     start_station_name as station,
     {{ dbt.date_trunc('month', 'started_at')}} as ride_month,
     {{ dbt.date_trunc('year', 'started_at')}} as ride_year,
+    rideable_type,
+    member_casual,
     sum(ride_length_sec) as monthly_total_ride_duration_sec,
     sum(ride_length_min) as monthly_total_ride_duration_min,
     avg(ride_length_sec) as monthly_avg_ride_duration_sec,
     avg(ride_length_min) as monthly_avg_ride_duration_min,
-    count(ride_id) as monthly_total_trips,
+    count(ride_id) as monthly_total_tripsm
+
 
     from rides_data
-    group by 1,2,3
+    group by 1,2,3,4,5
 
